31f832b20e353be57d69a1b9180a5e71924e2482
Dravid Joseph <dravidjoseph@gmail.com>
2016-03-27 20:15:56 -0500

    Add libraries for DiceRoll.java

---
+++ DiceRoll.java
@@ -0,0 +1,16 @@
+/**
+* @title DiceRoll.java
+* @author Dravid Joseph
+* @brief Contains code for Dice program
+*
+*/
+
+//Predefined libraries for graphics
+import javax.swing.*;
+import java.awt.Component;
+import java.awt.event.*;
+
+
+public class DiceRoll{
+	
+}
---
+++ DiceRollDriver.java
@@ -0,0 +1,14 @@
+/**
+* @title DiceRollDriver.java
+* @author Dravid Joseph
+* @brief Contains code to run Dice program
+*
+*/
+
+import javax.swing.*;
+import java.awt.Component;
+import java.awt.event.*;
+
+public class DiceRollDriver{
+	
+}
