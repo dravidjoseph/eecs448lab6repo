e07a498084e36f11b94c725e8b0fe613854c085f
Dravid Joseph <dravidjoseph@gmail.com>
2016-03-27 20:24:40 -0500

    Add DiceRoll constructor

--- DiceRoll.java
+++ DiceRoll.java
@@ -13,4 +13,31 @@ import java.awt.event.*;
 
 public class DiceRoll{
 	
+	private JPanel content;
+	private JButton roll;
+	private JLabel label;
+	private JLabel result;
+	private JTextField text;
+	
+	
+	public DiceRoll(){
+		content = new JPanel();
+		roll = new JButton("Roll");
+		label = new JLabel("How many faces does your die have?");
+		result = new JLabel("You rolled a: ");
+		
+		button.addActionListener(buttonListener());
+		
+		content.add(label);
+		content.add(text);
+		content.add(roll);
+		content.add(result);
+		
+		
+		
+	}
+
+	
+	
+	
 }
