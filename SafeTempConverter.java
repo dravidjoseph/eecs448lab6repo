f272f13d377e05adf368941034ff1751cde9fd78
Dravid Joseph <dravidjoseph@gmail.com>
2016-03-27 22:14:12 -0500

    Add constructor to load graphics

--- SafeTempConverter.java
+++ SafeTempConverter.java
@@ -6,6 +6,7 @@
 
 public SafeTempConverter{
 	
+	//declare instance variables
 	private JPanel content;
 	
 	private JPanel buttonPanel;
@@ -22,7 +23,46 @@ public SafeTempConverter{
 	private JLabel label;
 	private JLabel result;
 	
-	
+	public SafeTempConverter{
+		
+		//Load panels
+		content = newJPanel(new GridLayout(0,1));
+		buttonPanel = new JPanel(new GridLayout(2,3));
+		
+		fcButton = new JButton("F to C");
+		fkButton = new JButton("F to K");
+		cfButton = new JButton("C to F");
+		ckButton = new JButton("C to K");
+		kfButton = new JButton("K to F");
+		kcButton = new JButton("K to C");
+		
+		text = newJTextField(3);
+		
+		label = new JLabel("Enter a temperature to convert");
+		result = newJLabel("No temperature to convert.");
+		
+		//load listeners for each button
+		fcButton.addActionListener(fcButtonListener());
+		fkButton.addActionListener(fkButtonListener());
+		cfButton.addActionListener(cfButtonListener());
+		ckButton.addActionListener(ckButtonListener());
+		kfButton.addActionListener(kfButtonListener());
+		kcButton.addActionListener(kcButtonListener());
+		
+		//load button panel
+		buttonPanel.add(fcButton);
+		buttonPanel.add(fkButton);
+		buttonPanel.add(cfButton);
+		buttonPanel.add(ckButton);
+		buttonPanel.add(kfButton);
+		buttonPanel.add(kcButton);
+		
+		//load content
+		content.add(label);
+		content.add(text);
+		content.add(buttonPanel);
+		content.add(result);
+	}
 	
 	
 }
