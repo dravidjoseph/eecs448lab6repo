2de690f8780c4c0972fdcbb4c53687c9ca9ff507
Dravid Joseph <dravidjoseph@gmail.com>
2016-03-27 20:36:42 -0500

    Add .gitignore

--- DiceRollDriver.java
+++ DiceRollDriver.java
@@ -11,4 +11,17 @@ import java.awt.event.*;
 
 public class DiceRollDriver{
 	
+	private static void createAndDisplayGUI(){
+		JFrame frame = new JFrame("Dice");
+		DiceRoll dice = new DiceRoll();
+		
+		frame.getContentPane().add(dice.getContent());
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args){
+		createAndDisplayGUI();
+	}
+	
 }
