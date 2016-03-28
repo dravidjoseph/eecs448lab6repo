236d35faf31d33e04f4656a877735c8803a36a64
Dravid Joseph <dravidjoseph@gmail.com>
2016-03-27 22:07:43 -0500

    Add item instance variables

---
+++ SafeTempConverter.java
@@ -0,0 +1,28 @@
+/**
+* @title SafeTempConverter.java
+* @author Dravid Joseph
+* @brief Contains code to run Dice program
+*/
+
+public SafeTempConverter{
+	
+	private JPanel content;
+	
+	private JPanel buttonPanel;
+	
+	private JButton fcButton;
+	private JButton fkButton;
+	private JButton cfButton;
+	private JButton ckButton;
+	private JButton kfButton;
+	private JButton kcButton;
+	
+	private JTextField text;
+	
+	private JLabel label;
+	private JLabel result;
+	
+	
+	
+	
+}
---
+++ SafeTempConverterDriver.java
@@ -0,0 +1,5 @@
+/**
+* @title SafeTempConverterDriver.java
+* @author Dravid Joseph
+* @brief Contains code to run Dice program
+*/
